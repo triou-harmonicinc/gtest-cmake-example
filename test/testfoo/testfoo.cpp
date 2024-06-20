62f0944c7bc1f0c1f8d11d67eabbe6158e4de71f
Thibault Riou <thibault.riou@harmonicinc.com>
2024-06-20 14:51:07 +0000

    Add new test and use gtest_discover

--- test/testfoo/testfoo.cpp
+++ test/testfoo/testfoo.cpp
@@ -1,35 +1,45 @@
-#include "foo.h"
 #include "testfoo.h"
+#include "foo.h"
+#include "gtest/gtest.h"
 
 using ::testing::Return;
 
 FooTest::FooTest() {
-    // Have qux return true by default
-    ON_CALL(m_bar,qux()).WillByDefault(Return(true));
-    // Have norf return false by default
-    ON_CALL(m_bar,norf()).WillByDefault(Return(false));
+  // Have qux return true by default
+  ON_CALL(m_bar, qux()).WillByDefault(Return(true));
+  // Have norf return false by default
+  ON_CALL(m_bar, norf()).WillByDefault(Return(false));
 }
 
-FooTest::~FooTest() {};
+FooTest::~FooTest(){};
 
-void FooTest::SetUp() {};
+void FooTest::SetUp(){};
 
-void FooTest::TearDown() {};
+void FooTest::TearDown(){};
 
 TEST_F(FooTest, ByDefaultBazTrueIsTrue) {
-    Foo foo(m_bar);
-    EXPECT_EQ(foo.baz(true), true);
+  Foo foo(m_bar);
+  EXPECT_EQ(foo.baz(true), true);
 }
 
 TEST_F(FooTest, ByDefaultBazFalseIsFalse) {
-    Foo foo(m_bar);
-    EXPECT_EQ(foo.baz(false), false);
+  Foo foo(m_bar);
+  EXPECT_EQ(foo.baz(false), false);
 }
 
 TEST_F(FooTest, SometimesBazFalseIsTrue) {
-    Foo foo(m_bar);
-    // Have norf return true for once
-    EXPECT_CALL(m_bar,norf()).WillOnce(Return(true));
-    EXPECT_EQ(foo.baz(false), true);
+  Foo foo(m_bar);
+  // Have norf return true for once
+  EXPECT_CALL(m_bar, norf()).WillOnce(Return(true));
+  EXPECT_EQ(foo.baz(false), true);
 }
 
+class FooTest2 : public ::testing::TestWithParam<bool> {};
+
+TEST_P(FooTest2, Baz) {
+  MockBar bar;
+  Foo foo(bar);
+  EXPECT_EQ(foo.baz(GetParam()), GetParam());
+}
+
+INSTANTIATE_TEST_CASE_P(TrueAndFalse, FooTest2, ::testing::Values(true, false));
